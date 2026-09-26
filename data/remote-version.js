0d0b03a006666be23210a8acc8edef1956408da0
IELTS Passport <ielts-passport@example.com>
2026-09-26 17:50:21 +0800

    Refine app palette and release v1.0.8

--- data/remote-version.js
+++ data/remote-version.js
@@ -1,1 +1,1 @@
-window.IELTS_REMOTE_VERSION = {"webVersion":"1.0.7","releasedAt":"2026-09-26T08:54:27.603Z","siteUrl":"https://nma82439668hhf.github.io/ielts-passport/"};
+window.IELTS_REMOTE_VERSION = {"webVersion":"1.0.8","releasedAt":"2026-09-26T09:49:54.542Z","siteUrl":"https://nma82439668hhf.github.io/ielts-passport/"};
--- data/version.js
+++ data/version.js
@@ -1,2 +1,2 @@
 window.IELTS_BANK = window.IELTS_BANK || {};
-window.IELTS_BANK.version = {"webVersion":"1.0.7","releasedAt":"2026-09-26T08:54:27.603Z"};
+window.IELTS_BANK.version = {"webVersion":"1.0.8","releasedAt":"2026-09-26T09:49:54.542Z"};
